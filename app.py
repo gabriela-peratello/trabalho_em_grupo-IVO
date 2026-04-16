ece0cfea2e0f4727c91b66ce5b987a4bf7b5c5f4
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-04-16 14:58:57 -0300

    Acertando as rotas

--- app.py
+++ app.py
@@ -5,12 +5,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def pag_inicial():
-    return render_template("index.html, produtos = produtos")
+    produtos = []
+    return render_template("index.html", produtos=produtos)
 
-@app.route("/produto")
-def pag_dois():
-    return render_template("card.html")
+@app.route("/produto/<codigo>")
+def pag_dois(codigo):
+    try:
+        produto = recuperar_produto(codigo)
 
+        if not produto:
+            return "Produto não encontrado", 404
 
-app.run()
+        return render_template("produto.html", produto=produto)
 
+    except Exception as e:
+        return f"Erro: {e}"
+
+if __name__ == "__main__":
+    app.run(debug=True)

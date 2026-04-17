58a28e310e037957500e629744bbcbe143f89d15
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-04-17 11:24:42 -0300

    Fazendo tela de cadastro e login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 from model.produto import recuperar_produto
+from model.usuario import Usuario
 
 app = Flask(__name__)
 
@@ -20,6 +21,31 @@ def pag_dois(codigo):
 
     except Exception as e:
         return f"Erro: {e}"
+    
+
+@app.route("/cadastrar_usuario", methods= ["POST"])
+def cadastrar_usuario():
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+    nome = request.form.get("nome")
+
+    novo_usuario = Usuario(usuario, senha, nome)
+    novo_usuario.cadastrar()
+
+    return redirect("/")
+
+@app.get("/cadastro")
+def cadastro_login():
+    return render_template("cadastro.html")
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ model/usuario.py
@@ -0,0 +1,16 @@
+from database.conexao import conectar
+
+class Usuario:
+    def __init__(self, usuario:str, senha:str, nome:str=None):
+        self.usuario = usuario
+        self.senha = senha
+        self.nome = nome 
+
+    def cadastrar(self):
+        conexao, cursor = conectar()
+        cursor.execute("""
+                    INSERT INTO usuarios (usuario, senha, nome)
+                    VALUES (%s, %s, %s);
+                    """, [self.usuario, self.senha, self.nome])
+        conexao.commit()
+        conexao.close()
